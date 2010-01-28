888116c23786869cfba9c03be111ce762135e9de
moa.bluebugs <moa.bluebugs@957bb544-b276-11de-89e1-bde060728117>
2010-01-28 17:02:17 +0000

    * jeux/helper/edje-helper.edj, 	* jeux/helper/ecore-job-helper.js, 	* jeux/helper/Makefile: Make ecore_job_* optionnal.

--- trunk/jeux/helper/ecore-job-helper.js
+++ trunk/jeux/helper/ecore-job-helper.js
@@ -1,51 +1,54 @@
 var ecore_event_job_type = 0;
 
-function ecore_job_add(func_handler, func_free, data)
-{
-   function _ecore_job_event_handler(data, type, ev)
+if (ecore_job_add === undefined)
+  {
+     function ecore_job_add(func_handler, func_free, data)
      {
-        ev.funch(ev.data);
-        return 1;
+	function _ecore_job_event_handler(data, type, ev)
+	{
+	   ev.funch(ev.data);
+	   return 1;
+	}
+	function _ecore_job_event_free(data, ev)
+	{
+	   if (ev.funcf)
+	     ev.funcf(ev.data);
+	}
+
+	var job;
+
+	if (!func_handler) return null;
+	if (!ecore_event_job_type)
+	  {
+	     ecore_event_job_type = ecore_event_type_new();
+	     ecore_event_handler_add(ecore_event_job_type, _ecore_job_event_handler, null);
+	  }
+	job = new Object();
+
+	job.event = ecore_event_add(ecore_event_job_type, job, _ecore_job_event_free, null);
+	if (!job.event)
+	  return null;
+
+	job.funch = func_handler;
+	job.funcf = func_free;
+	job.data = data;
+	return job;
      }
-   function _ecore_job_event_free(data, ev)
+
+     function ecore_job_del(job)
      {
-	if (ev.funcf)
-          ev.funcf(ev.data);
+	var data;
+
+	data = job.data;
+
+	if (job.event === undefined)
+	  return null;
+
+	ecore_event_del(job.event);
+
+	return data;
      }
-
-   var job;
-
-   if (!func_handler) return null;
-   if (!ecore_event_job_type)
-     {
-        ecore_event_job_type = ecore_event_type_new();
-        ecore_event_handler_add(ecore_event_job_type, _ecore_job_event_handler, null);
-     }
-   job = new Object();
-
-   job.event = ecore_event_add(ecore_event_job_type, job, _ecore_job_event_free, null);
-   if (!job.event)
-     return null;
-
-   job.funch = func_handler;
-   job.funcf = func_free;
-   job.data = data;
-   return job;
-}
-
-function ecore_job_del(job)
-{
-   var data;
-
-   data = job.data;
-
-   if (job.event === undefined)
-     return null;
-
-   ecore_event_del(job.event);
-
-   return data;
-}
+  }
 
 true;
 
