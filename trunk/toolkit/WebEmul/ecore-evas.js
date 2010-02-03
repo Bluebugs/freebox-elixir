8933269992066d14eef5be293e3cffbadc5bab2b
papa2enfants <papa2enfants@957bb544-b276-11de-89e1-bde060728117>
2010-02-03 22:54:12 +0000

    Ajout d'un émulateur Web d'Elixir (pour FF, Chrome, etc...) dans l'émulateur 'screen' devient 'Screen' à cause de l'objet DOM existant.

---
+++ trunk/toolkit/WebEmul/ecore-evas.js
@@ -0,0 +1,18 @@
+function ecore_evas_init() {
+  if (VERBOSE) elx.print("ecore_evas_init()\n");
+}
+
+function ecore_evas_new() {
+  if (VERBOSE) elx.print("ecore_evas_new()\n");
+  return [];
+}
+
+function ecore_evas_get() {
+  if (VERBOSE) elx.print("ecore_evas_get()\n");
+  return [];
+}
+
+function ecore_evas_show() {
+  if (VERBOSE) elx.print("ecore_evas_show()\n");
+  return [];
+}
