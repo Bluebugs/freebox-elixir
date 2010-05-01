7ac54d59831f11ef0a3de7858bfbf1ffdf6b3355
brouits <brouits@957bb544-b276-11de-89e1-bde060728117>
2010-05-01 23:20:03 +0000

    switched to IPv6

--- applications/mpd-client/db.js
+++ applications/mpd-client/db.js
@@ -3,10 +3,14 @@ var db_name = "MPD-client.db";
 var tbl_name = null;
 
 var id = "1"; /* db primary key (only one mpd anyway) */
-var host1 = 192;
-var host2 = 168;
+var host1 = 0;
+var host2 = 0;
 var host3 = 0;
-var host4 = 1;
+var host4 = 0;
+var host5 = 0;
+var host6 = 0;
+var host7 = 0;
+var host8 = 0;
 var port = 6600; /* tcp port */
 
 function db_init(name)
@@ -25,10 +29,10 @@ function db_init(name)
   }
 
   var obj = {};
-  sqlite3_exec(db, "SELECT host1, host2, host3, host4 FROM " + tbl_name + "WHERE id='" + id + "';", test_func, obj);
+  sqlite3_exec(db, "SELECT host1, host2, host3, host4, host5, host6, host7, host8 FROM " + tbl_name + "WHERE id='" + id + "';", test_func, obj);
   if (!obj.ok) {
-    sqlite3_exec(db, "CREATE TABLE " + name + " (id INTEGER PRIMARY KEY, host1 INTEGER NOT NULL, host2 INTEGER NOT NULL, host3 INTEGER NOT NULL, host4 INTEGER NOT NULL, port INTEGER);", null, null);
-    sqlite3_exec(db , "INSERT INTO " + name + " (id, host1, host2, host3, host4, port) VALUES ('" + id + "', '" + host1 + "', '" + host2 + "', '" + host3 + "', '" + host4 + "', '" + port + "');", null, null);
+    sqlite3_exec(db, "CREATE TABLE " + name + " (id INTEGER PRIMARY KEY, host1 INTEGER NOT NULL, host2 INTEGER NOT NULL, host3 INTEGER NOT NULL, host4 INTEGER NOT NULL, host5 INTEGER NOT NULL, host6 INTEGER NOT NULL, host7 INTEGER NOT NULL, host8 INTEGER NOT NULL, port INTEGER);", null, null);
+    sqlite3_exec(db , "INSERT INTO " + name + " (id, host1, host2, host3, host4, host5, host6, host7, host8, port) VALUES ('" + id + "', '" + host1 + "', '" + host2 + "', '" + host3 + "', '" + host4 + "', '" + host5 + "', '" + host6 + "', '" + host7 + "', '" + host8 + "', '" + port + "');", null, null);
   }
 
   return true;
@@ -36,7 +40,7 @@ function db_init(name)
 
 function db_save(pref)
 {
-  sqlite3_exec(db , "REPLACE INTO " + tbl_name + " (id, host1, host2, host3, host4, port) VALUES (" + id + ", " + pref.host1 + ", " + pref.host2 + ", " + pref.host3 + ", " + pref.host4 + ", " + pref.port + ");", null, null);
+  sqlite3_exec(db , "REPLACE INTO " + tbl_name + " (id, host1, host2, host3, host4, host5, host6, host7, host8, port) VALUES (" + id + ", " + pref.host1 + ", " + pref.host2 + ", " + pref.host3 + ", " + pref.host4 + ", " + pref.host5 + ", " + pref.host6 + ", " + pref.host7 + ", " + pref.host8 + ", " + pref.port + ");", null, null);
 }
 
 function db_load()
@@ -44,11 +48,11 @@ function db_load()
   var result = [];
 
   function get_row(arr, row) {
-    arr[row.id] = { id: row.id, host1: row.host1, host2: row.host2, host3: row.host3, host4: row.host4, port: row.port };
+    arr[row.id] = { id: row.id, host1: row.host1, host2: row.host2, host3: row.host3, host4: row.host4, host5: row.host5, host6: row.host6, host7: row.host7, host8: row.host8, port: row.port };
     return 0;
   }
 
-  sqlite3_exec(db, "SELECT id, host1, host2, host3, host4, port FROM " + tbl_name + " WHERE id='" + id +"';", get_row, result);
+  sqlite3_exec(db, "SELECT id, host1, host2, host3, host4, host5, host6, host7, host8, port FROM " + tbl_name + " WHERE id='" + id +"';", get_row, result);
   return result;
 }
 
