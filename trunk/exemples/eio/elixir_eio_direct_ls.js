74c12e41bfaecdfe3a67a3d03191d1fb92841d91
moa.bluebugs <moa.bluebugs@957bb544-b276-11de-89e1-bde060728117>
2010-10-27 12:20:42 +0000

    * eio: add sample code that show asynchronous file listing.

---
+++ trunk/exemples/eio/elixir_eio_direct_ls.js
@@ -0,0 +1,22 @@
+elx.load("eio");
+elx.load("ecore");
+elx.load("mix");
+
+ecore_init();
+eio_init();
+
+eio_filter_add("Sample OGG");
+
+tst = eio_file_direct_ls(".",
+                         function (data, file) { elx.print("FILTER: ", file.path, "\n"); return true; },
+                         function (data, file) { elx.print("FILE: ", file.path, "\n"); },
+                         function (data) { elx.print("DONE\n"); ecore_main_loop_quit(); },
+                         function (error, data) { elx.print("ERR: ", error, "\n"); ecore_main_loop_quit(); },
+                         null);
+
+ecore_main_loop_begin();
+
+eio_filter_del("Sample OGG");
+
+eio_shutdown();
+ecore_shutdown();
